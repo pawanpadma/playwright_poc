7e6d19fdad774566029c111463c211fdeb4e788b
Pawan Kumar Padma <pawanpadma@gmail.com>
2025-04-06 18:13:57 +0530

    Update home-page.ts

--- playwright_automation/pageobjects/home-page.ts
+++ playwright_automation/pageobjects/home-page.ts
@@ -26,7 +26,7 @@ export class HomePage extends BasePage {
         this.logger.log('Performing action...');
       }
 
-    function subtractOneMonth(dateString: string): string {
+     subtractOneMonth(dateString: string): string {
     const [year, month, day] = dateString.split("-").map(Number);
     const date = new Date(year, month - 1, day);
     date.setMonth(date.getMonth() - 1);
