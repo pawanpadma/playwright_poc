4995a1b4cd99be41b6ddda51f201d0cb659cfbf6
Pawan Kumar Padma <pawanpadma@gmail.com>
2025-04-21 08:34:17 +0530

    Checkout code

--- playwright_automation/playwright.config.ts
+++ playwright_automation/playwright.config.ts
@@ -1,28 +1,26 @@
 import { defineConfig, devices } from '@playwright/test';
 import * as process from 'process';
 
+const reporters: any[] = [
+  ['list'], // CLI output
+  ['html', { outputFolder: 'playwright-report', open: 'never' }],
+];
+
+// Conditionally add JSON reporter if env variable is set
+if (process.env.REPORT_JSON) {
+  reporters.unshift(['json', { outputFile: process.env.REPORT_JSON }]);
+}
+
 export default defineConfig({
   testDir: './tests',
   timeout: 20 * 1000,
-  workers:2,
-  
-  
-
- 
- 
-  reporter: [
-    ['json', { outputFile: 'test-results/test-results.json' }],
-    ['list'], // CLI output
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    
-  ],
+  workers: 2,
+  fullyParallel: true,
   outputDir: 'test-results',
 
-
-
+  reporter: reporters,
 
   use: {
-    //baseURL: 'https://www.saucedemo.com',
     baseURL: 'https://deal4loans.com',
     headless: true,
     actionTimeout: 90 * 1000,
@@ -36,47 +34,38 @@ export default defineConfig({
       name: 'chrome',
       use: {
         browserName: 'chromium',
-        // Browser-specific options
         channel: 'chrome',
-        headless:false
+        headless: false,
       },
     },
     /*
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    }, */
-
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     browserName: 'firefox',
-    //     headless:false
-    //   },
-    // },
-
-   /*
+    },
+    {
+      name: 'firefox',
+      use: {
+        browserName: 'firefox',
+        headless: false
+      },
+    },
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
     },
-
     {
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
     },
-
     {
       name: 'Microsoft Edge',
       use: { ...devices['Desktop Edge'], channel: 'msedge' },
     },
     */
   ],
-  fullyParallel:true
-  
 });
