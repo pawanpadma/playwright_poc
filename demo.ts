33296cb48a5e0c6df7147add38a418d514d50aa2
Pawan Kumar Padma <pawanpadma@gmail.com>
2026-02-24 08:14:48 +0530

    Update demo.ts

--- demo.ts
+++ demo.ts
@@ -1,6 +1,12 @@
 import { test as base } from '@playwright/test';
 
-export const test = base.extend({});
+//export const test = base.extend({});
+export const test = base.extend({
+  accessToken: async ({}, use) => {
+    const token = await getAccessToken();
+    await use(token);
+  }
+});
 
 // Global hooks for entire project
 test.beforeAll(async () => {
@@ -11,9 +17,16 @@ test.afterAll(async () => {
   console.log('>>> AFTER ALL TESTS IN FILE');
 });
 
-test.beforeEach(async ({ page }) => {
-  console.log('>>> BEFORE EACH TEST');
+//test.beforeEach(async ({ page }) => {
+//  console.log('>>> BEFORE EACH TEST');
   // Example: Navigate to base URL
+//  await page.goto('/');
+//});
+test.beforeEach(async ({ page, accessToken }) => {
+  await page.addInitScript((token) => {
+    localStorage.setItem('access_token', token);
+  }, accessToken);
+
   await page.goto('/');
 });
 
